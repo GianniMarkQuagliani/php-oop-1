a0afa6a13f000ca2467dcf090887a7a7dcdb57ea
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-06 13:45:02 +0100

    scaffolding

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.css' integrity='sha512-r0fo0kMK8myZfuKWk9b6yY8azUnHCPhgNz/uWDl2rtMdWJlk7gmd9socvGZdZzICwAkMgfTkVrplDahQ07Gi0A==' crossorigin='anonymous'/>
+    
+    <!-- Title -->
+    <title>Movie</title>
+</head>
+<body>
+
+    <!-- Container -->
+    <div  class="container">
+        <h1>Movie!</h1>
+    </div>
+
+</body>
+</html>
