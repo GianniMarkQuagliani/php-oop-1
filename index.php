3ab2a5f2102e6de91d2cb2ecef83f915fb51b81d
GianniMarkQuagliani <giannimarkquagliani@gmail.com>
2023-11-07 01:15:27 +0100

    add search

--- index.php
+++ index.php
@@ -3,6 +3,27 @@
 require_once __DIR__ . '/model/movie.php';
 require_once __DIR__ . '/db/db.php';
 
+// Search 
+if (isset($_GET['search'])) {
+    // Questo è utile per rendere la ricerca case-insensitive, in modo che la ricerca funzioni sia con maiuscole che con minuscole.
+    $searchQuery = strtolower($_GET['search']);
+    // Questo è un array vuoto che verrà utilizzato per contenere i film che corrispondono ai criteri di ricerca.
+    $filteredMovies = [];
+
+    foreach ($movies as $movie) {
+        // strago i dettagli del film (titolo, regista, generi) in formato di stringa e li converto in lettere minuscole.
+        $movieDetails = strtolower($movie->getDetails());
+
+        //  verifico se la stringa di ricerca ($searchQuery) è contenuta nei dettagli del film ($movieDetails). La funzione strpos restituirà la posizione in cui inizia la corrispondenza nella stringa o false se non trova una corrispondenza.
+        if (strpos($movieDetails, $searchQuery) !== false) {
+            // il film viene aggiunto all'array $filteredMovies
+            $filteredMovies[] = $movie;
+        }
+    }
+
+    // Sovrascrivo la lista dei film con i risultati filtrati
+    $movies = $filteredMovies;
+}
 
 ?>
 
@@ -17,7 +38,9 @@ require_once __DIR__ . '/db/db.php';
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>    <!-- Title -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- Font Awesome -->
+    <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
     <!-- Title -->
     <title>Movie List</title>
 </head>
@@ -45,7 +68,18 @@ require_once __DIR__ . '/db/db.php';
     </nav>
     <!-- Container -->
     <div  class="container">
-        <h1 class="mt-5">Movie List</h1>
+    <h1 class="mt-5">Movie List</h1>
+    <form class="form-inline mt-3">
+    <div class="input-group">
+        <input class="form-control" type="text" name="search" placeholder="Search">
+        <div class="input-group-append">
+            <button class="btn btn-primary ms-2" type="submit">
+                <i class="fas fa-search"></i> Search
+            </button>
+        </div>
+    </div>
+</form>
+
         <div class="row mt-4">
             <?php foreach ($movies as $movie): ?>
                 <div class="col-md-4 mb-4">
